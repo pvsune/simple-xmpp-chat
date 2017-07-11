e8eb943413c0c754ec26ee1097a21b137762f392
Philamer Sune <phil@pez.ai>
2017-07-11 18:31:44 +0800

    Add strophe.js echobot; remove conversejs client.

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from bottle import route, run, template
+from bottle import get, route, run, static_file, template
 
 
 @route('/')
 def index():
     return template('index.html', {'url': 'http://localhost:5280/http-bind'})
+
+
+@get("/static/js/<filepath:re:.*\.js>")
+def js(filepath):
+    return static_file(filepath, root="static/js")
 
 run(host='0.0.0.0', port=8080)
