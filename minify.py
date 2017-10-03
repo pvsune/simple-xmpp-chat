b57a39568e61ab6611ce6e039d91fc386e8d8c09
Harrison Dano <harrisondano@pez.ai>
2017-10-03 15:45:46 +0800

    Feat/Prod: Debugging

--- minify.py
+++ minify.py
@@ -8,11 +8,14 @@ files = ['pez_widget', 'pez_widget_main']
 folder = 'public/'+env+'/js/'
 
 for js in files:
-	minified = requests.post(url, data={'input': open(folder+js+'.js', 'rb').read()})
-	f = open(folder+js+'.min.js','w')
+	source = folder+js+'.js'
+	target = folder+js+'.min.js'
+	minified = requests.post(url, data={'input': open(source, 'rb').read()})
+	f = open(target,'w')
 	f.seek(0)
-	f.write(minified)
+	f.write(minified.text.encode('utf-8'))
 	f.truncate()
 	f.close()
+	print 'minified '+target
 
-print done
+print 'done'
